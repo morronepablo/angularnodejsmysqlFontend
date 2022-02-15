2fa6103310f092f45485190103d36931f3e3c034
morronepablo <morronepablo@gmail.com>
2022-02-15 18:25:53 -0300

    cracion de lectura y cracion de registros

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,28 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CreateComponent } from './create/create.component';
+import { ReadComponent } from './read/read.component';
+
+import { HttpClientModule } from '@angular/common/http';
+import { ApiserviceService } from './apiservice.service';
+
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CreateComponent,
+    ReadComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [ApiserviceService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/create/create.component.ts
@@ -0,0 +1,42 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { ApiserviceService } from '../apiservice.service';
+
+@Component({
+  selector: 'app-create',
+  templateUrl: './create.component.html',
+  styleUrls: ['./create.component.css']
+})
+export class CreateComponent implements OnInit {
+
+  constructor(private service: ApiserviceService) { }
+
+  errormsg:any;
+  successmsg:any;
+
+  ngOnInit(): void {
+  }
+
+  userForm = new FormGroup({
+
+    'fullname': new FormControl('', Validators.required),
+    'email': new FormControl('', Validators.required),
+    'mobile': new FormControl('', Validators.required)
+
+  });
+
+  userSubmit() {
+    if(this.userForm.valid) {
+      console.log(this.userForm.value);
+      this.service.createData(this.userForm.value).subscribe((res) => {
+        console.log(res, 'res==>');
+        this.userForm.reset();
+        this.successmsg = res.message;
+      });
+    } else {
+      this.errormsg = 'Todos los campos son requeridos !'
+    }
+
+  }
+
+}
